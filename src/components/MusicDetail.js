73c28e2eef5e0e446f243f31586f5c780d2c3e4e
DeepCFoxXx <learncode23@gmail.com>
2018-03-14 14:37:11 +0000

    added music detail

---
+++ src/components/MusicDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MusicDetail = (props) => {
+  if(!props.song) return null;
+  console.log(prop.song["link"][1]["attributes"]["href"]);
+  return (
+    <div className='song'>
+    <h3>{props.song["im:name"]["label"]}</h3>
+    <h4>{props.song["im:artist"]["label"]}</h4>
+    <img src={props.song["im:image"][2]["label"]}/>
+    <audio id="audio" key={props.song["im:name"]["label"]} controls>
+     <source src={props.song["link"][1]["attributes"]["href"]} type="audio/mp4"/>
+    </audio>
+  </div>
+  )
+}
+
+export default MusicDetail;
