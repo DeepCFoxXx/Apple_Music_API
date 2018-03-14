058111d232c3e1188572f3f4fb05ca39b8521f5d
DeepCFoxXx <learncode23@gmail.com>
2018-03-14 13:48:19 +0000

    added render in music selector

--- src/containers/MusicContainer.js
+++ src/containers/MusicContainer.js
@@ -11,29 +11,39 @@ class MusicContainer extends React.Components {
     };
     this.handleMusicSelected = this.handleMusicSelected.bind(this);
   }
-}
 
-componentDidMount() {
-  const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', () => {
-    if(xhr.status !== 200) return;
-    const jsonString = xhr.responseText;
-    const data = JSON.parse(jsonString);
-    this.setState({
-      music: data.feed.entry
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', () => {
+      if(xhr.status !== 200) return;
+      const jsonString = xhr.responseText;
+      const data = JSON.parse(jsonString);
+      this.setState({
+        music: data.feed.entry
+      });
     });
-  });
 
-  xhr.send();
-}
+    xhr.send();
+  }
 
-handleMusicSelected(index) {
-  const selectedMusic = this.state.music[index];
-  this.setState({
-    currentMusic: selectedMusic
-  });
+  handleMusicSelected(index) {
+    const selectedMusic = this.state.music[index];
+    this.setState({
+      currentMusic: selectedMusic
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>Apple Top 20</h2>
+        <MusicSelector music={this.state.music} onMusicSelected={this.handleMusicSelected}/>
+        <MusicDetail song={this.state.currentMusic}/>
+      </div>
+    );
+  }
 }
 
 export default MusicContainer;
