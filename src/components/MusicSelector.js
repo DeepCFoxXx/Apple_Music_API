24288ab08e4b690883a49a067ef6012caeff1113
DeepCFoxXx <learncode23@gmail.com>
2018-03-15 12:28:59 +0000

    started music selector class

---
+++ src/components/MusicSelector.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MusicDetail from './MusicDetail.js';
+import MusicContainer from '../containers/MusicContainer.js';
+
+const MusicSelector = (props) => {
+
+}
+
+export default MusicSelector;
