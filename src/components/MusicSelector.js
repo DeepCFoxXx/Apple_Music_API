8627d686779faa4b7f77e1c0dde331f5eeb71197
DeepCFoxXx <learncode23@gmail.com>
2018-03-15 12:33:38 +0000

    added song + index map

--- src/components/MusicSelector.js
+++ src/components/MusicSelector.js
@@ -4,6 +4,14 @@ import MusicContainer from '../containers/MusicContainer.js';
 
 const MusicSelector = (props) => {
 
+  function handleChange(e) {
+    props.onMusicSelected(e.target.value);
+  }
+
+  const options = props.music.map((song, index) => {
+    return <option value={index} key={index}>{song["im:name"]["label"]}</option>
+  });
+
 }
 
 export default MusicSelector;
